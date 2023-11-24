509b42d6dde10bd985dccbb52c1720f65606c61c
AfilAhamed <afilahamed0007@gmail.com>
2023-11-24 17:30:51 +0530

    Feeds Screen Completed

--- lib/view/feeds_screen.dart/feeds_screen.dart
+++ lib/view/feeds_screen.dart/feeds_screen.dart
@@ -5,23 +5,45 @@ class FeedsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Row(children: [
-          const Text(
-            'Hot & New',
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
-          ),
-          const Spacer(),
-          IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.cast,
-                size: 30,
-              ))
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Row(children: [
+            const Text(
+              'Hot & New',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+            ),
+            const Spacer(),
+            IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.cast,
+                  size: 30,
+                ))
+          ]),
+          bottom: TabBar(
+              isScrollable: true,
+              labelColor: Colors.black,
+              unselectedLabelColor: Colors.white,
+              labelStyle:
+                  const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              indicator: BoxDecoration(
+                  color: Colors.white, borderRadius: BorderRadius.circular(50)),
+              tabs: const [
+                Tab(
+                  text: "🍿 Coming Soon",
+                ),
+                Tab(
+                  text: "👀 Everyone's Watching",
+                )
+              ]),
+        ),
+        body: TabBarView(children: [
+          Text('1'),
+          Text('2'),
         ]),
       ),
-      body: Text('feeds'),
     );
   }
 }
