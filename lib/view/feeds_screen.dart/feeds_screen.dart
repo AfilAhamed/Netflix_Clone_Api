fb44f5f2078f3f3d4aafa3c3009ea17125c79e5b
AfilAhamed <afilahamed0007@gmail.com>
2023-11-24 16:09:45 +0530

    Home Screen Completed

--- lib/view/feeds_screen.dart/feeds_screen.dart
+++ lib/view/feeds_screen.dart/feeds_screen.dart
@@ -6,6 +6,21 @@ class FeedsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Row(children: [
+          const Text(
+            'Hot & New',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+          ),
+          const Spacer(),
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.cast,
+                size: 30,
+              ))
+        ]),
+      ),
       body: Text('feeds'),
     );
   }
