0cb3cd44aeac4c941fb3d9fc1e239c5668ef9bbd
AfilAhamed <afilahamed0007@gmail.com>
2023-11-25 14:26:23 +0530

    Api Integration Started

---
+++ lib/model/tmdb_model.dart
@@ -0,0 +1,16 @@
+class MovieInfoModel {
+  String title;
+  String posterPath;
+  String releaseDate;
+  String orginalTitle;
+  String overview;
+
+  MovieInfoModel({
+    required this.title,
+    required this.posterPath,
+    required this.releaseDate,
+    required this.orginalTitle,
+    required this.overview,
+
+  })
+}
